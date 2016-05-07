d12d49c142f4dea2c27f52fe5e1e829f49ebb408
Satinder Grewal <satinder@Satinders-MacBook-Pro.local>
2016-05-07 12:17:15 +1200

    enabled dev tools in electron-iguana

--- electron-iguana/main.js
+++ electron-iguana/main.js
@@ -30,7 +30,7 @@ function createWindow () {
   //mainWindow.loadURL('file://' + __dirname + '/index.html')
   mainWindow.loadURL('http://localhost:3000/#/')
   // Open the DevTools.
-  //mainWindow.webContents.openDevTools()
+  mainWindow.webContents.openDevTools()
 
   // Emitted when the window is closed.
   mainWindow.on('closed', function () {
